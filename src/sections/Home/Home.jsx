680041e933fe35cf2aa7bd30c1b2ef154dc2ff45
Alex Zuart <jgordillozuart@gmail.com>
2023-11-14 21:08:00 -0600

    resolve

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,21 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Portada from "../Portada/Portada.jsx"
-import ListaPeliculas from "../moviesList/moviesList.jsx";
 import Menu from "../Menu/menu.jsx";
-//import Button from 'react-bootstrap/Button';
-
-
 export default function Home(){
     return(
         <>
-           {/* MENU, Portada, Lista de peliculas, footer */} 
-           {/*<Portada />*/}
-           {/*<ListaPeliculas/>*/}
            <Menu />
            <Outlet />
-
         </>
-
     )
 }
 
